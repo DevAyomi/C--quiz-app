77c0c7b40df1fed6610dcedffdf59ac70e82367b
Devayomi <gbolagadewinner@gmail.com>
2024-09-20 11:20:36 +0100

    Color added

--- Quiz.cs
+++ Quiz.cs
@@ -13,7 +13,19 @@ namespace QuizApp
 
         public void DisplayQuestion(Question question)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("||===================================================================||");
+            Console.WriteLine("||                             Questions                             ||");
+            Console.WriteLine("||===================================================================||");
+            Console.ResetColor();
             Console.WriteLine(question.QuestionText);
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write($" {i + 1}. ");
+                Console.ResetColor();
+                Console.WriteLine($"{question.Answers[i]}");
+            }
         }
     }
 }
--- obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be4d33ad2d120a7d39262713e9f7001d7168169")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
